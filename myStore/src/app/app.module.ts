94da0fa43cb79c111c440fa4df33b26bb1b2e4fc
lk220203006@126.com <lk220203006@126.com>
2019-08-05 15:39:14 +0800

    update

--- myStore/src/app/app.module.ts
+++ myStore/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -9,6 +10,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
    declarations: [
@@ -17,15 +19,18 @@ import { CartComponent } from './cart/cart.component';
       ProductListComponent,
       ProductAlertsComponent,
       ProductDetailsComponent,
-      CartComponent
+      CartComponent,
+      ShippingComponent
    ],
    imports: [
       BrowserModule,
+      HttpClientModule,
       ReactiveFormsModule,
       RouterModule.forRoot([
          { path: '', component: ProductListComponent },
          { path: 'products/:productId', component: ProductDetailsComponent },
          { path: 'cart', component: CartComponent },
+         { path: 'shipping', component: ShippingComponent },
       ])
    ],
    providers: [],
